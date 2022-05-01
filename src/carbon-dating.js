7735220f94e43c9d5197f5979e43a07a36309279
doorisclosed <polikarpov-sereg@mail.ru>
2022-05-01 22:00:50 +0300

    feat: comlete carbon-dating task

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,18 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function dateSample(sampleActivity) {
+  if ((typeof(sampleActivity)) == 'string') {
+  let dating = Math.log(MODERN_ACTIVITY / sampleActivity) * HALF_LIFE_PERIOD / Math.log(2);
+
+    if (!isNaN(parseFloat(sampleActivity)) && isFinite(sampleActivity)) {
+      return Math.ceil(dating);
+    } else {
+      return false;
+    }
+  } else {
+    return false;
+  }
 }
 
 module.exports = {
